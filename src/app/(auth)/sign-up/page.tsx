e0aa24db5ca70ffe7a842f2537705d7ca506ef20
kajal-jotwani <kajaljotwani06@gmail.com>
2025-12-02 00:44:52 +0530

    feat: add sign-in sign-up frontend

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,25 @@
+import { SignupForm } from "@/shadcn-components/signup-form"
+import Image from "next/image"
+
+export default function LoginPage() {
+  return (
+    <div className="relative min-h-svh flex flex-col items-center justify-center p-6 md:p-10 overflow-hidden">
+      {/* Background image */}
+      <div className="absolute inset-0 -z-10">
+        <Image
+          src="/checkboard.png"
+          alt="Checkboard Background"
+          fill
+          className="object-cover"
+          quality={100}
+          priority
+        />
+      </div>
+
+      <div className="w-full max-w-sm md:max-w-4xl relative z-10">
+        <SignupForm/>
+      </div>
+    </div>
+  )
+}
+
